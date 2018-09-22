3b44fdf7b2b11383ca75cfcf2ac780c57a014562
Zhang Zhe <zhe.zhang@stega.ca>
2018-09-22 16:42:49 -0400

    more clean.

--- script.js
+++ script.js
@@ -1,101 +1,76 @@
-function validateForm(form){
+function validateForm(form) {
 
 
-      var input_username =form.username.value;
-      var input_password=document.getElementById('password').value;
-      var error=0;
-      //console.log(input_username.length);
-    
-  
- if (input_username.length < 8 ) {
-        document.getElementById('username').style.borderColor ="red";
-        document.getElementById('form_usernameError').style.display="block";
-        console.log(input_username.length);
-        error++;
-        console.log("error"+error);
-       
- }
- else if (input_username.length > 25) {
-        document.getElementById('username').style.borderColor ="red";
-        document.getElementById('form_usernameError').style.display="block";
-        error++;
-        console.log(input_username.length);
-        console.log("error"+error);
-        
- }
-else{
-    document.getElementById('username').style.borderColor ="transparent";
-    document.getElementById('form_usernameError').style.display="none";
-    console.log(input_username.length);
+    var input_username = form.username.value;
+    var username_id = 'username';
+    var username_error_id = 'form_usernameError';
+    var input_password = form.password.value;
+    var password_id = 'password';
+    var password_error_id = 'form_passwordError';
 
-    console.log("error"+error);
 
+    var error = 0;
+    var error1 = 0;
+    error = input_validtaion(input_username, username_id, username_error_id, error);
+    error1 = input_validtaion(input_password, password_id, password_error_id, error1);
+    console.log("error" + error);
+    console.log("error" + error1);
+    error = error + error1;
+
+
+    if (error == 0) {
+        document.getElementById('login').style.display = "none";
+        document.getElementById('loader').style.display = "block";
+
+        setTimeout(function () {
+            document.getElementById('login-col').style.display = "none";
+            document.getElementById('logedin').style.display = "table-cell";
+        }, 3000);
+    }
+
+    return false;
 }
 
- if(input_password.length < 8){
-      document.getElementById('password').style.borderColor ="red";
-      document.getElementById('form_passwordError').style.display="block";
-      error++;
-      console.log(input_password.length);
-      console.log("error1"+error);
-     
- }
-else if(input_password.length > 25 ){
-    document.getElementById('password').style.borderColor ="red";
-    document.getElementById('form_passwordError').style.display="block";
-    error++;
-    console.log(input_password.length);
-    console.log("error1"+error);
-    
-}
-else{
-    document.getElementById('password').style.borderColor ="transparent";
-    document.getElementById('form_passwordError').style.display="none";
-    console.log(input_password.length);
 
-    console.log("error1"+error);
-
-}
-
-if (error == 0){
-    document.getElementById('login').style.display="none";
-    document.getElementById('loader').style.display="block";
-
-    setTimeout(function(){    
-        document.getElementById('login-col').style.display="none";
-        document.getElementById('logedin').style.display="table-cell"; 
-    }, 3000);
-}
-
- /*
- else{
-     var submit_button = document.getElementById('login');
-     var loader=document.getElementById('loader');
-      $("#loader").show();
-      $("#login").replaceWith(loader);
-      return true;
- }
-   */
-  return false;
-}
-/*
-$( "#signout" ).click(function() {
-    document.getElementById('login-col').style.display="table-cell"; 
-    $( "#logedin" ).hide();
-    $( "#loader" ).hide();
-    $( "#login" ).show();
-    $("#login-form").trigger("reset");
-    $( "#checkbox-1" ).prop( "checked", false );
-  });
-*/
 document.getElementById("signout").addEventListener("click", signoutFunction);
 
 function signoutFunction() {
-    document.getElementById('login-col').style.display="table-cell";
-    document.getElementById('logedin').style.display="none";
-    document.getElementById('loader').style.display="none";
-    document.getElementById('login').style.display="table-cell";
+    document.getElementById('login-col').style.display = "table-cell";
+    document.getElementById('logedin').style.display = "none";
+    document.getElementById('loader').style.display = "none";
+    document.getElementById('login').style.display = "table-cell";
     document.getElementById("login-form").reset();
     document.getElementById("checkbox-1").checked = false;
+
+}
+
+function input_validtaion(input_value, input_field, error_message, count) {
+
+    if (input_value.length < 8) {
+        document.getElementById(input_field).style.borderColor = "red";
+        document.getElementById(error_message).style.display = "block";
+        //console.log(input_username.length);
+        count++;
+        //console.log("error"+count);
+
+
+    } else if (input_value.length > 25) {
+        document.getElementById(input_field).style.borderColor = "red";
+        document.getElementById(error_message).style.display = "block";
+        count++;
+        //console.log(input_username.length);
+        //console.log("error"+error);
+
+
+    } else {
+        document.getElementById(input_field).style.borderColor = "transparent";
+        document.getElementById(error_message).style.display = "none";
+        //console.log(input_username.length);
+        //console.log("error"+error);
+        count = 0;
+
+
+    }
+    return count;
 
 }
